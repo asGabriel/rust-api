dd6bee4986ede4ca589e43a88bf9556d585c07dc
gabriel <andrade.gabriel2295@gmail.com>
2026-08-03 05:13:55 -0300

    fix: update matchmaking.rs wiring missed by the Session rename commit

--- api/src/modules/matchmaking.rs
+++ api/src/modules/matchmaking.rs
@@ -4,7 +4,7 @@ use axum::Router;
 
 use crate::modules::{
     matchmaking::handler::{
-        game_day::DynGameDayHandler, played_match::DynPlayedMatchHandler, player::DynPlayerHandler,
+        played_match::DynPlayedMatchHandler, player::DynPlayerHandler, session::DynSessionHandler,
         team::DynTeamHandler,
     },
     routes::AppState,
@@ -17,7 +17,7 @@ pub mod routes;
 
 pub struct MatchmakingState {
     pub player_handler: Arc<DynPlayerHandler>,
-    pub game_day_handler: Arc<DynGameDayHandler>,
+    pub session_handler: Arc<DynSessionHandler>,
     pub team_handler: Arc<DynTeamHandler>,
     pub played_match_handler: Arc<DynPlayedMatchHandler>,
 }
@@ -28,7 +28,7 @@ pub fn configure_service_routes() -> Router<AppState> {
         Router::new()
             .merge(routes::configure_routes())
             .merge(routes::player::configure_routes())
-            .merge(routes::game_day::configure_routes())
+            .merge(routes::session::configure_routes())
             .merge(routes::team::configure_routes())
             .merge(routes::played_match::configure_routes()),
     )
